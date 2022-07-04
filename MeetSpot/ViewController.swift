154eb022c88665230e6ea707f2b2c6a5cf5a9f2e
b150005 <b150005@outlook.jp>
2022-07-04 20:08:10 +0900

    Initial Commit

---
+++ MeetSpot/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MeetSpot
+//
+//  Created by 伊藤 直輝 on 2022/07/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
